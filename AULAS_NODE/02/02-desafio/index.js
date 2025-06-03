28b420f24144921acf3c0d92f786dc2cf1f0c9ca
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2025-06-02 22:10:23 -0300

    feat: modulo de calculo mais organizado!

--- AULAS_NODE/02/02-desafio/index.js
+++ AULAS_NODE/02/02-desafio/index.js
@@ -1,3 +1,12 @@
     const meuModulo = require('./meumodulo');
-    const soma = meuModulo.soma 
-    soma(2, 5); //10
+    const soma = meuModulo.soma;
+    soma(2, 5); //7
+
+    const sub = meuModulo.sub;
+    sub(5, 2); //3
+
+    const multi = meuModulo.multi;
+    multi(2, 5); //10
+
+    const div = meuModulo.div;
+    div(10, 2); //5
--- AULAS_NODE/02/02-desafio/meumodulo.js
+++ AULAS_NODE/02/02-desafio/meumodulo.js
@@ -1,7 +1,19 @@
 module.exports = {
 
 soma(a, b) {
+    return console.log(a + b);
+},
+
+sub(a, b) {
+    return console.log(a - b);
+},
+
+multi(a, b) {
     return console.log(a * b);
+},
+
+div(a, b) {
+    return console.log(a / b);
 }
 
 }
