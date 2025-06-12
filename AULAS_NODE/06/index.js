23175972e77a596f247eb10fd3f1fc08ce92e6cd
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2025-06-11 22:03:32 -0300

    feat: cadastro e edição da lista dos livros com pagina estilizada

--- AULAS_NODE/06/index.js
+++ AULAS_NODE/06/index.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql = require('mysql2')
-
+ 
 const app = express()
-
+ 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
-
-app.use(express.urlencoded({
-    extended: true
-}))
+ 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+ 
 app.use(express.json())
-// Middleware para servir arquivos estáticos
+ 
 app.use(express.static('public'))
-// Configuração do Handlebars
+ 
 app.get('/', function (req, res) {
-    res.render('home')
+  res.render('home')
 })
-
-// Rota para guardar livros
+ 
 app.post('/books/insertbook', function (req, res) {
   const title = req.body.title
   const pageqty = req.body.pageqty
@@ -33,8 +35,7 @@ app.post('/books/insertbook', function (req, res) {
     res.redirect('/')
   })
 })
-
-// Rota para exibir o formulário de inserção de livros
+ 
 app.get('/books', function (req, res) {
   const query = `SELECT * FROM books`
  
@@ -49,9 +50,8 @@ app.get('/books', function (req, res) {
  
     res.render('books', { books })
   })
-});
-
-// Rota para exibir o formulário de inserção de livros
+})
+ 
 app.get('/books/:id', function (req, res) {
   const id = req.params.id
  
@@ -69,17 +69,70 @@ app.get('/books/:id', function (req, res) {
     res.render('book', { book })
   })
 })
-
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql',
-});
-conn.connect(function (err) {
+ 
+app.get('/books/edit/:id', function (req, res) {
+  const id = req.params.id
+ 
+  const query = `SELECT * FROM books WHERE id = ${id}`
+ 
+  conn.query(query, function (err, data) {
     if (err) {
-        console.log(err)
+      console.log(err)
     }
-    console.log('Conectado ao MySQL!')
-    app.listen(3000)
-});
+ 
+    const book = data[0]
+ 
+    console.log(data[0])
+ 
+    res.render('editbook', { book })
+  })
+})
+ 
+app.post('/books/updatebook', function (req, res) {
+  const id = req.body.id
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+ 
+  const query = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`
+ 
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+ 
+    res.redirect(`/books/edit/${id}`)
+  })
+})
+ 
+app.post('/books/remove/:id', function (req, res) {
+  const id = req.params.id
+ 
+  const query = `DELETE FROM books WHERE id = ${id}`
+ 
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+ 
+    res.redirect(`/books`)
+  })
+})
+ 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysql',
+})
+ 
+conn.connect(function (err) {
+  if (err) {
+    console.log(err)
+  }
+ 
+  console.log('Conectado ao MySQL!')
+ 
+  app.listen(3000)
+})
+ 
+
